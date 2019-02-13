8823a845dad8f5e0b9877f2bd5591663b9e2a2db
Society Project <46810642+TheSocietyProject@users.noreply.github.com>
2019-02-13 10:39:25 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import com.github.steveice10.mc.protocol.data.message.ChatColor;
+import com.github.steveice10.mc.protocol.data.message.Message;
+import com.github.steveice10.mc.protocol.data.message.MessageStyle;
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.sasha.eventsys.SimpleEventHandler;
 import com.sasha.eventsys.SimpleListener;
+import com.sasha.reminecraft.ReMinecraft;
 import com.sasha.reminecraft.api.RePlugin;
 import com.sasha.reminecraft.api.event.ChatReceivedEvent;
+import com.sasha.reminecraft.client.ChildReClient;
 import com.sasha.reminecraft.client.ReClient;
 import com.sasha.reminecraft.logging.ILogger;
 import com.sasha.reminecraft.logging.LoggerBuilder;
 
+import javax.swing.event.ChangeListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Style;
+import java.util.Enumeration;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -47,29 +60,29 @@ private boolean inQueue = true;
 
     }
 
-    public void timeOutAction(){
-        reconnect(2000);
-
-    }
-
     public boolean queueTimeOut(){
         /*
             the other time outs r in testWetherInQueue:
-                Position in queue: 0
-                Null time out sth... // TODO
+                - Position in queue: 0
+                - Exception Connecting:ReadTimeoutException : null
 
          */
 
         long now = System.currentTimeMillis();
-
         return now - lastMsg > acceptedMsgTime && !inQueue;
     }
 
+    public void timeOutAction(){
+        reconnect();
 
-    public void reconnect(int millis){
-        // TODO test wether this works + add sleep(millis)
+    }
+
+
+
+
+    public void reconnect(){
+        // TODO search for a better way! also with millis to wait
         this.getReMinecraft().reLaunch();
-
     }
 
 
@@ -96,7 +109,7 @@ private boolean inQueue = true;
             return inQueue = true;
 
 
-        if(msg.equals("Null sth ")){ // TODO
+        if(msg.equals("Exception Connecting:ReadTimeoutException : null")){ // TODO
             timeOutAction();
             return inQueue = true;
         }
